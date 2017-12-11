6fbb1e4bf8cbc4c5839460955d2430a8750415b0
龙龙 <longlong@scy.local>
2017-12-11 09:32:11 +0800

    Initial Commit

---
+++ Student news/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Student news
+//
+//  Created by 龙龙 on 2017/12/11.
+//  Copyright © 2017年 龙龙. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
